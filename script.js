d0cba6864966e00bdafc087b8eb5909abf51e333
pranjalkarn9-art <pranjalkarn9@gmail.com>
2026-09-26 19:29:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,70 +2,56 @@ const games = [
 
     {
         title: "Guess the Number",
-
         description:
             "Test your guessing skills and find the secret number.",
-
         link:
             "https://pranjalkarn9-art.github.io/Guess-The-Number/"
     },
 
     {
         title: "Aim the Target",
-
         description:
             "Test your accuracy and reflexes by hitting targets.",
-
         link:
             "https://pranjalkarn9-art.github.io/Aim-The-Target/"
     },
 
     {
         title: "Math Quest",
-
         description:
             "Turn mathematics into a fun gaming challenge.",
-
         link:
             "https://pranjalkarn9-art.github.io/mathquest/"
     },
 
     {
         title: "Code Breaker",
-
         description:
             "Crack the hidden code using logic and deduction.",
-
         link:
             "https://pranjalkarn9-art.github.io/Code-Cracker/"
     },
 
     {
         title: "Mystery Detective",
-
         description:
             "Investigate clues and solve mysterious cases.",
-
         link:
             "https://pranjalkarn9-art.github.io/mystery-detective/"
     },
 
     {
         title: "Memory Match",
-
         description:
             "Test your memory, find matching pairs and build powerful combos.",
-
         link:
             "https://pranjalkarn9-art.github.io/Match-The-Card/"
     },
 
     {
         title: "Asteroid Blaster",
-
         description:
             "Blast asteroids, survive dangerous waves and battle powerful space bosses.",
-
         link:
             "https://pranjalkarn9-art.github.io/Asteroid-Blaster/"
     }
@@ -83,16 +69,13 @@ $("themeBtn").onclick = () => {
 
     document.body.classList.toggle("light");
 
-
     const light =
         document.body.classList.contains("light");
-
 
     $("themeBtn").textContent =
         light
             ? "☀️"
             : "🌙";
-
 
     localStorage.setItem(
         "gameZoneTheme",
@@ -130,18 +113,14 @@ $("randomBtn").onclick = () => {
             )
         ];
 
-
     $("randomTitle").textContent =
         random.title;
-
 
     $("randomDescription").textContent =
         random.description;
 
-
     $("randomLink").href =
         random.link;
-
 
     $("randomPopup")
         .classList
